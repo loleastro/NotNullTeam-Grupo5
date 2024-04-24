374aaf336fe518cb3f85a365ac2f3b24c61a8507
Franco Matias Nicotra <franco.nicotra@mercadolibre.com>
2024-04-24 12:28:08 -0300

    Add endpoint getFollowersCount

--- src/main/java/org/mercadolibre/NotNullTeam/service/impl/SellerServiceImpl.java
+++ src/main/java/org/mercadolibre/NotNullTeam/service/impl/SellerServiceImpl.java
@@ -1,6 +1,9 @@
 package org.mercadolibre.NotNullTeam.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.mercadolibre.NotNullTeam.DTO.response.SellerFollowersCountDto;
+import org.mercadolibre.NotNullTeam.exception.error.NotFoundException;
+import org.mercadolibre.NotNullTeam.model.Seller;
 import org.mercadolibre.NotNullTeam.repository.ISellerRepository;
 import org.mercadolibre.NotNullTeam.service.ISellerService;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SellerServiceImpl implements ISellerService {
     final ISellerRepository iSellerRepository;
+
+    @Override
+    public SellerFollowersCountDto getFollowersCount(Long id) {
+        Seller seller = iSellerRepository.findById(id).orElseThrow(() -> new NotFoundException("Seller"));
+
+        int followersCount = seller.quantityOfFollowers();
+
+        return new SellerFollowersCountDto(seller.getUser().getId(), seller.getUser().getName(), followersCount);
+    }
 }
